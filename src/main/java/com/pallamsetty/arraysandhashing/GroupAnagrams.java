f56076701435384e60ce71e63752427c2f51f024
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2025-03-19 11:20:11 -0400

    ArraysAndHashing: Group Anagrams| Added a faster method and tests

--- src/main/java/com/pallamsetty/arraysandhashing/GroupAnagrams.java
+++ src/main/java/com/pallamsetty/arraysandhashing/GroupAnagrams.java
@@ -4,6 +4,7 @@ package com.pallamsetty.arraysandhashing;
 * Leetcode 49
 * */
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -21,6 +22,34 @@ public class GroupAnagrams {
             anagramMap.putIfAbsent(sortedCurrent, new ArrayList<>());
             anagramMap.get(sortedCurrent).add(s);
         }
+        return new ArrayList<>(anagramMap.values());
+    }
+
+    public List<List<String>> groupAnagramsWithoutSorting(String[] strs) {
+        HashMap<String, List<String>> anagramMap = new HashMap<>();
+
+        for(String s: strs) {
+            /* Instead of sorting and using the sorted values as the key,
+                we are using a count array of 26 elements which keep track of occurrence
+                or each character in the word. This will be the key as all the anagrams
+                have the same array representation of occurrences of characters.
+                This will be a constant space allocation per word. On top of that, the
+                other space allocation will be of the hashmap which O(m) where m is
+                the number of strings in the array. Time complexity depends on the longest
+                string length. If it is n, this is the time it takes to populate the
+                count array. This operation will be repeated m times. So the time
+                complexity will be O(m*n) */
+            int[] count = new int[26];
+
+            for(char c: s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            String key = Arrays.toString(count);
+            anagramMap.putIfAbsent(key, new ArrayList<>());
+            anagramMap.get(key).add(s);
+        }
+
         return new ArrayList<>(anagramMap.values());
     }
 }
--- src/test/java/com/pallamsetty/arraysandhashing/GroupAnagramsTest.java
+++ src/test/java/com/pallamsetty/arraysandhashing/GroupAnagramsTest.java
@@ -59,6 +59,51 @@ public class GroupAnagramsTest {
         assertLists(expected, actual);
     }
 
+    @Test
+    public void testGraoupAnagramsWithoutSorting1() {
+        String[] strs = { "eat","tea","tan","ate","nat","bat" };
+        List<List<String>> expected = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        current.add("ate");
+        current.add("eat");
+        current.add("tea");
+        expected.add(current);
+        current = new ArrayList<>();
+        current.add("bat");
+        expected.add(current);
+        current = new ArrayList<>();
+        current.add("nat");
+        current.add("tan");
+        expected.add(current);
+        List<List<String>> actual = ga.groupAnagramsWithoutSorting(strs);
+
+        assertLists(expected, actual);
+    }
+
+    @Test
+    public void testGroupAnagramsWithoutSorting2() {
+        String[] strs = { "" };
+        List<List<String>> expected = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        current.add("");
+        expected.add(current);
+        List<List<String>> actual = ga.groupAnagramsWithoutSorting(strs);
+
+        assertLists(expected, actual);
+    }
+
+    @Test
+    public void testGroupAnagramsWithoutSorting3() {
+        String[] strs = { "a" };
+        List<List<String>> expected = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        current.add("a");
+        expected.add(current);
+        List<List<String>> actual = ga.groupAnagramsWithoutSorting(strs);
+
+        assertLists(expected, actual);
+    }
+
     private void assertLists(List<List<String>> expected, List<List<String>> actual) {
         assertEquals(expected.size(), actual.size());
         HashSet<List<String>> expectedhashSet = new HashSet<>();
